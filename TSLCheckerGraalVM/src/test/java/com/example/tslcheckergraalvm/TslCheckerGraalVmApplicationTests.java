8975912213330350f60c1d667667d661542eb75f
Szabolcs Andras Szogyi <Andras-Szabolcs.Szogyi@telekom.com>
2024-05-31 15:31:19 +0200

    GraalVM; SpringBoot

---
+++ TSLCheckerGraalVM/src/test/java/com/example/tslcheckergraalvm/TslCheckerGraalVmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tslcheckergraalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TslCheckerGraalVmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
