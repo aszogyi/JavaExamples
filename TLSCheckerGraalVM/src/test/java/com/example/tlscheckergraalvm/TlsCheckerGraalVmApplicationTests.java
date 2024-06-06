6ca58fd1a0f4fa6ac5f6507b4b55ed8a9c29026e
Szabolcs Andras Szogyi <Andras-Szabolcs.Szogyi@telekom.com>
2024-06-06 09:26:28 +0200

    TLS Checker; GraalVM; Gradle kotlin; SpringBoot

---
+++ TLSCheckerGraalVM/src/test/java/com/example/tlscheckergraalvm/TlsCheckerGraalVmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tlscheckergraalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlsCheckerGraalVmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
